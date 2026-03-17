4bebbad67cb2e7a0c67cb9e73591397f01e9cf95
kejuanling <1365126894@qq.com>
2026-03-17 16:39:50 +0800

    修正

--- src/main/java/com/itheima/reggie/config/WebMvcConfig.java
+++ src/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -1,12 +1,14 @@
 package com.itheima.reggie.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+@Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
-        registry.addResourceHandler("/font/**").addResourceLocations("classpath:/font/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
     }
 }
