20f8af816e510a48d4b2add03525157c8f318285
kejuanling <1365126894@qq.com>
2026-04-07 16:43:55 +0800

    添加，修改员工信息

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -12,6 +12,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -76,4 +77,57 @@ public class EmployeeController {
 
         return R.success(pageInfo);
     }
+
+    /**
+     * 新增员工：一级路径是employee，没有二级路径
+     * @param employee
+     * @return
+     */
+    @PostMapping
+    public R save(@RequestBody Employee employee,HttpSession session){
+        log.info("新增员工,员工信息:{}",employee.toString());
+        //1.设置员工状态
+        employee.setStatus(1);
+        //2.设置创建和更新时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        //3.获取员工id
+        Long userId = (Long) session.getAttribute("employee");
+        employee.setCreateUser(userId);
+        employee.setUpdateUser(userId);
+        //4.设置密码
+        String password=DigestUtils.md5DigestAsHex("000000".getBytes());
+        employee.setPassword(password);
+        //5.保存到数据库
+        employeeService.save(employee);
+        //6.返回成功信息
+        return R.success(employee+"新增员工成功");
+    }
+    /**
+     * 员工信息回显：一级路径是employee,二级没有
+     * 请求方式：get；请求参数：路径参数，员工id
+     */
+    @GetMapping("/{id}")
+    public R<Employee> getById(@PathVariable Long id){
+        Employee employee = employeeService.getById(id);
+        if(employee != null){
+            return R.success(employee);
+        }
+       return R.error("没有查询到员工信息");
+    }
+
+    /**
+     * 修改员工信息
+     * 请求方式：put；请求参数：json
+     */
+    @PutMapping
+    public R<String> update(@RequestBody Employee employee,HttpSession session){
+        //1.修改上传时间和修改人员
+        employee.setUpdateTime(LocalDateTime.now());
+        Long userid =(Long) session.getAttribute("employee");
+        employee.setUpdateUser(userid);
+        employeeService.updateById(employee);
+        return R.success("修改成功");
+    }
+
 }
